4ed693b992f3f46d8c9bc575b26989e9a55e43ee
CarsickCoder9 <165290214+CarsickCoder9@users.noreply.github.com>
2024-03-30 12:14:17 +0000

    container config done

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer code //
+// alert(3+3) //we are using nodejs not browser thus if we use it we will get error //
+
+let name = "siya"
+let age = 18
+let isLoogIn = true
+
+// number => 2 to power 53
+// bigInt => 
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assinged yet
+// symbol => unique 
+// object =>   
+
+// console.log(typeof "siya")// string 
+console.log(typeof null)// object 
+console.log(typeof undifiend) //undifiend 
+
+
+
+
+
+
+
