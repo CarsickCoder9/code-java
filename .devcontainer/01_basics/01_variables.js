97db00ee2c01573bb1dff0be8d4e108ee611d635
CarsickCoder9 <165290214+CarsickCoder9@users.noreply.github.com>
2024-03-30 11:00:52 +0000

    hello

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = "14455"
+let accountEmail = "siya@.com"
+var accountPssword = "12345"
+accountCity = "amravati"
+let accountState;
+
+console.log(accountId);
+
+accountEmail = "hc@vc.com"
+accountPssword = "1212"
+accountCity = "pune"
+
+console.table([ accountId, accountEmail, accountPssword, accountCity, accountState]);
