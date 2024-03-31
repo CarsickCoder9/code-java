990e89c432ca33548b924ca6f02d3574095eb0cc
CarsickCoder9 <165290214+CarsickCoder9@users.noreply.github.com>
2024-03-31 10:27:45 +0000

    done

--- .devcontainer/01_basics/03_conversionOpration.js
+++ .devcontainer/01_basics/03_conversionOpration.js
@@ -28,10 +28,46 @@ let IsLoggedIn = " siya"
 //"siya" = true
 
 let someNumber = 33
-console.log(typeof (someNumber));
+//console.log(typeof (someNumber));
 
-let stringNumber = String(someNumber);
+//let stringNumber = String(someNumber);
 
 
 
-console.log(typeof (stringNumber));
+///////////******************************************OPERATION*************************************///////////
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+
+let str1 = "hello"
+let str2 = " siya"
+
+let str3 = str1 + str2
+
+//console.log(str3);
+
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//console.log(+true); 
+//console.log(+"");
+//both conversions are not prefered //
+
+//let num1 + num2 + num3
+//num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++;
+
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
