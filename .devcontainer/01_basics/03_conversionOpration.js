fdd2a317b0c30a5ec5928be606eb170665d9c5b8
CarsickCoder9 <165290214+CarsickCoder9@users.noreply.github.com>
2024-03-31 08:57:03 +0000

    container config done

---
+++ .devcontainer/01_basics/03_conversionOpration.js
@@ -0,0 +1,37 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let ValueInNumber = Number(score);
+
+// console.log(typeof ValueInNumber); 
+// console.log(ValueInNumber);
+
+// "33" = 33
+// "33adc" NaN
+// null = 0
+// true = 1 
+// false = 0
+// undefiend = NaN
+
+let IsLoggedIn = " siya"
+// console.log(typeof IsLoggedIn);
+
+ let booleanIsLoggedIn = Boolean(IsLoggedIn);
+
+// console.log(typeof (booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+
+// 1 = true ;  0 = false
+//" " = false
+//"siya" = true
+
+let someNumber = 33
+console.log(typeof (someNumber));
+
+let stringNumber = String(someNumber);
+
+
+
+console.log(typeof (stringNumber));
