56d148544ae0a5a1236446d59f3a69a094ff5b1d
CarsickCoder9 <165290214+CarsickCoder9@users.noreply.github.com>
2024-04-02 11:32:23 +0000

    memory in js

---
+++ .devcontainer/01_basics/datatype-summary.js
@@ -0,0 +1,52 @@
+// primitive
+
+// 7 types : string , number , boolean , null , undefined , symbol , bigIn
+
+const score = 100 
+const scorevalue = 100.3
+
+const isLoogIn = false
+const outsideTemp = null
+let userEmail;
+
+let Id = Symbol ('123')
+let accountId = Symbol ('123')
+
+console.log(Id === accountId);
+
+// referance (non primitive)
+
+// arrey , object , functions
+
+//const heros = ["ironman" , "spiderman" , "shaktiman"];
+
+//{
+//    name = "siya",
+//    age = 13,
+//}
+ 
+//const myfunction = Function (){
+ //   console.log("hello world")
+//
+
+
+
+//stack (primitive) ; heap (non-primitive)
+
+let MyYoutubeName = "siyaArt"
+let anotherName = MyYoutubeName
+anotherName = "chai aur code"
+
+console.log(anotherName);
+console.log(MyYoutubeName);
+
+
+
+
+
+
+
+
+
+
+
