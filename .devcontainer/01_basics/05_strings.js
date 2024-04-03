a52583cce433a3cc77346c639066c34083c6e0e1
CarsickCoder9 <165290214+CarsickCoder9@users.noreply.github.com>
2024-04-03 13:40:14 +0000

    strings

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,50 @@
+const name = "siya"
+const repocount = 2 
+
+//console.log(name + repocount + "value"); avoid this code
+
+//console.log(`hello my name is ${name} my repo count is ${repocount}`);
+
+const gameName = new String('siya-hc-com-lom');
+
+//console.log(gameName[0]);
+//console.log(gameName[2]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.charAt(4));
+//console.log(gameName.toUpperCase());
+//console.log(gameName.toLowerCase());
+
+//console.log(gameName.indexOf('c'));
+
+const newstring = gameName.substring(0, 5);
+
+//console.log(newstring);
+
+const anotherstring = gameName.slice(-5, 3)
+
+//console.log(anotherstring);
+
+const newstringOne = "      siya     "
+//console.log(newstringOne);
+///console.log(newstringOne.trim());
+
+const url = "http://siya.com/siya%20jadhao"
+
+
+//console.log(url.replace('%20' , '-'));
+
+//console.log(url.includes('siya'));
+//console.log(url.includes('diya'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
