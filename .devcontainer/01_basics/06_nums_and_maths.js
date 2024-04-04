76925f9bdc09b6072ec1de5d0e877e21acef3cf5
CarsickCoder9 <165290214+CarsickCoder9@users.noreply.github.com>
2024-04-04 12:05:26 +0000

    nums and maths in js

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,61 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(400)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+//console.log(balance.construtor());
+
+
+const othernumber = 12.345
+console.log(othernumber.toPrecision(3));
+
+
+const hundreds = 100000000000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+///++++++++++++++++++++++++++++++++++++  MATHS ++++++++++++++++++++++++++++++++++++++++
+
+
+//console.log(Math);
+//console.log(Math.abs(-13456));
+//console.log(Math.round(123.64));
+//console.log(Math.ceil(4.9));
+//console.log(Math.floor(6.8));
+//console.log(Math.min(2835 , 7467324 ,5756, 32.435, 41742, 925.2));
+//console.log(Math.max(23,43,43,84,456,462,55,46,479,9568,956,5));
+
+
+//console.log(Math.random());
+//console.log((Math.random()* 10) + 1 );
+//console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
